67409afc0d3a35ef07299b7d4b95efe0c2b4ce27
carlota sas <casaor@outlook.com>
2021-02-03 19:05:32 +0100

    base

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div id="app">
+
+    <img alt="Vue logo" src="./assets/logo.png">
+
+    <modalBootstrap>
+
+      <template v-slot:header>
+        <h5 class="modal-title">Modal con slot</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </template>
+
+      <template v-slot:body>
+        <p>100$ son 81.41€</p>
+      </template>
+
+      <template v-slot:footer>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cierra</button>
+        <button type="button" class="btn btn-primary">Guarda cambios</button>
+      </template>
+
+    </modalBootstrap>
+
+  </div>
+
+</template>
+
+<script type="text/javascript" src="./app.js"></script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ModalBootstrap.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="modal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <slot name="header"></slot>
+        </div>
+        <div class="modal-body">
+            <slot name="body"></slot>
+        </div>
+        <div class="modal-footer">
+            <slot name="footer"></slot>
+        </div>
+        </div>
+    </div>
+    </div>
+
+</template>
